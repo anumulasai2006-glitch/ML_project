1555d218e9a9907f25c68d5b21aef7ac370b1bef
Sai Sandeep <anumulasai2006@gmail.com>
2026-06-17 17:28:42 +0530

    Update data ingestion and add notebook

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,11 +17,10 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
-        def initiate_data_ingestion(self):
-            logging.info("Entered the data ingestion method or component")
-            try:
-                df=pd.read_csv('ML_Project/notebook/data/stud.csv')
-
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+                df = pd.read_csv('notebook/data/stud.csv')
                 logging.info("Read the dataset as dataframe")
 
                 os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
@@ -42,7 +41,7 @@ class DataIngestion:
                     self.ingestion_config.test_data_path
                 )
 
-            except Exception as e:
+        except Exception as e:
                 raise CustomException(e, sys)
 
 if __name__=="__main__":
